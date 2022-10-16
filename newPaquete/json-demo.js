7cd399e1b4fc4026ae7f22d30cb3754c8d44332a
Anthony Maldonado <anthony@clay.cl>
2022-10-16 17:34:24 -0400

    working with JSON

---
+++ newPaquete/json-demo.js
@@ -0,0 +1,24 @@
+const curso = require('./curso.json');
+// console.log(...curso.temas);
+// console.log(curso.temas);
+
+let infoCurso = {
+    "tittle": "aprende Node.js",
+    "numVistas": 2,
+    "numLikes": 21123,
+    "temas": [
+        "JavaScript",
+        "Node.js",
+        "NPM",
+        "JSON"
+    ],
+    "esPublico": true
+}
+
+console.log(typeof(infoCurso));
+let infoJSON = JSON.stringify(infoCurso);
+console.log(typeof(infoJSON));
+
+let toJsonAObject = JSON.parse(infoJSON);
+console.log(toJsonAObject);
+console.log(typeof toJsonAObject);
