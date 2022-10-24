e71c8496247e7f1f96cf5cf7e2729ac12685f3d1
Anthony Maldonado <anthony@clay.cl>
2022-10-23 21:10:00 -0400

    trabajando con URL
    entendiendo el funcionamiento del método new URL

---
+++ newPaquete/url.js
@@ -0,0 +1,9 @@
+const miUrl = new URL('https://www.ejemplo.org/cursos/programacion?ordenar=vistas&nivel=1');
+
+console.log(miUrl)//esto es un objeto con la url "parseada" y podemos acceder a sus parametros
+console.log(miUrl.host)
+console.log(miUrl.pathname)
+console.log(miUrl.search)
+console.log(miUrl.searchParams)
+console.log(miUrl.searchParams.get("ordenar")) // retorna null si ese parametro no existe
+console.log(miUrl.searchParams.get("nivel"))
